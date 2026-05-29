c838c66457670aa166a533afe2071b4259a3023a
BARANY <ahmedbrany01020304@gmail.com>
2026-05-29 20:15:28 +0300

    rename app class to NoteApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NoteApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
 
   // This widget is the root of your application.
   @override
