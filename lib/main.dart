05fa7d63f719675ab1282cee549c0b06fe6b3713
BARANY <ahmedbrany01020304@gmail.com>
2026-06-16 00:49:49 +0300

    font added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/notes_view.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
+
   runApp(const NoteApp());
 }
 
@@ -15,6 +17,17 @@ class NoteApp extends StatelessWidget {
         brightness: Brightness.dark,
         //scaffoldBackgroundColor: Colors.white
       ),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+      ),
+    //  initialRoute: "/",
+
       home: NotesView(),
     );
   }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -13,8 +13,8 @@ class NotesView extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 24),
           child: const Column(
             children: [
-              CustomAppBar()
-
+              CustomAppBar(),
+              OneNoteWidget()
             ],
           ),
         ),
@@ -22,3 +22,43 @@ class NotesView extends StatelessWidget {
     );
   }
 }
+class OneNoteWidget extends StatelessWidget {
+  const OneNoteWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      padding: EdgeInsets.symmetric(vertical: 20),
+      decoration: BoxDecoration(
+        color: Colors.orangeAccent,
+        borderRadius: BorderRadius.circular(8)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ListTile(
+            title: Text("FLutter Tips",style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 28
+            ),),
+            subtitle: Text("Tharwat samy give us an advice ",style: TextStyle(
+              fontWeight: FontWeight.w100,
+              color: Colors.black,
+              fontSize: 18
+            ),),
+            trailing: Icon(Icons.delete_forever,color: Colors.black,),
+          ),Align(
+            alignment:AlignmentGeometry.bottomRight ,
+            child: Text("2025-20-20",style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w100
+            ),),
+          )
+        ],
+      ),
+    );
+  }
+}
+
